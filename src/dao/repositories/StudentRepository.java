b42957e29f941a8f98511df21b60b89352b119b2
Kush Dhawan <kushkumardhawan@gmail.com>
2020-04-21 19:12:11 +0530

    Native Quieries Testing Done

--- src/dao/repositories/StudentRepository.java
+++ src/dao/repositories/StudentRepository.java
@@ -2,6 +2,7 @@ package dao.repositories;
 
 import java.util.List;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
@@ -12,8 +13,9 @@ import entities.Student;
 @Repository
 public interface StudentRepository extends PagingAndSortingRepository<Student, Integer> {
 
-	@Query("from Student")
-	List<Student> findAllStudents();
+	@Query(value= "from Student", nativeQuery=false)
+	List<Student> findAllStudents();  
+	//List<Student> findAllStudents(Pageable pageable);   if we want to add the Pagination and Sorting
 	
 	@Query("select st.firstName, st.lastName from Student st")
 	List<Object[]> findAllPartialStudentsData();
